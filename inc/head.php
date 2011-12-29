801b440093d5c5c46553d46febc8a3fe45fff71d
John Costanzo <jrock2004@gmail.com>
2011-12-29 09:52:12 -0500

    Starting new folder structure

---
+++ inc/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>HyperSpin Cabinet</title>
+    <link rel="stylesheet" href="styles/master.css" type="text/css" media="screen" charset="utf-8">
+    <?php include('config.inc'); ?>
+    <?php //include('inc/functions.php'); ?>
+</head>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <?php include('inc/head.php'); ?>
+    <body>
+        <div id="outer">
+            <div id="header">
+                <h1 id="siteTitle"><?php echo $owner; ?></h1>
+            </div>
+            <div id="container">
+                <div id="content">
+                    <?php //listRoms(); ?>
+                </div>
+                <div id="sidebar">
+                    <div id="serverStatus">
+                        <h3>Server Status</h3>
+                        <ul>
+                            <li>HyperSpin: <?php //checkHyperSpin(); ?></li>
+                        </ul>
+                    </div>
+                    <div id="emulators">
+                        <h3>Emulators</h3>
+                        <ul>
+                            <?php //listOfEmulators(); ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="footer">
+                Footer
+            </div>
+        </div>
+    </body>
+</html>
